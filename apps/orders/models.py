cc63e6a47490e5abecee9cad7fdce6d9d58d7820
Temur <temurxasanov320@gmail.com>
2026-06-01 20:39:24 +0500

    models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 from apps.common.models import BaseModel
+from django.contrib.auth.models import User
 
 class Order(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.CharField(max_length=255)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    address = models.CharField(max_length=255)
+
     def __str__(self):
         return f"Order of {self.product} (Quantity: {self.quantity})"
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 from apps.common.models import BaseModel
 
 # Create your models here.
 
 class UserProfile(BaseModel):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
 
     def __str__(self):
